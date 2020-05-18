34c824a95891c198be7633b4fbe2d6ab7ec9939e
Zakharov Roman <zakharovroman@hotmail.com>
2020-05-18 21:26:11 +0300

    1

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Роман Захаров on 18.05.2020.
+//  Copyright © 2020 Роман Захаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
